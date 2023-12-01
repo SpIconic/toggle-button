7bfbc476e151b7411b130de31d0c30729e2c55c0
HspIconic <151531152+SpIconic@users.noreply.github.com>
2023-12-01 23:46:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let body = document.querySelector('body');
+let toggle = document.querySelector('.toggle');
+
+toggle.onclick = function(){
+    body.classList.toggle('active');
+    toggle.classList.toggle('active');
+}
